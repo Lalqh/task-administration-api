6e5e1a5eebb5920d92b6878c7c715c18cb5f3863
lalqh <lalincotero13@gmail.com>
2026-01-21 01:05:59 -0600

    feat: add TypeOrmModule imports for TaskLog in TaskLogsModule and TasksModule

--- src/task-logs/task-logs.module.ts
+++ src/task-logs/task-logs.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TaskLogsService } from './task-logs.service';
+import { TaskLog } from './entities/task-log.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TaskLog])],
   providers: [TaskLogsService],
 })
 export class TaskLogsModule {}
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
-import { TasksController } from './tasks.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksService } from './tasks.service';
+import { TasksController } from './tasks.controller';
+import { Task } from './entities/task.entity';
+import { Tag } from './entities/tag.entity';
+import { User } from 'src/users/user.entity';
+import { TaskLog } from 'src/task-logs/entities/task-log.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Task, Tag, User, TaskLog])],
   controllers: [TasksController],
-  providers: [TasksService]
+  providers: [TasksService],
+  exports: [TasksService],
 })
 export class TasksModule {}
