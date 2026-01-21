923dc9c0beb2a00177ffc29647a1c81df35763ce
lalqh <lalincotero13@gmail.com>
2026-01-21 01:07:51 -0600

    fix: correct import path for Task entity and update Tag entity properties

--- src/tasks/entities/tag.entity.ts
+++ src/tasks/entities/tag.entity.ts
@@ -1,14 +1,17 @@
 import { Entity, PrimaryGeneratedColumn, Column, ManyToMany } from 'typeorm';
-import { Task } from '../../tasks/entities/task.entity';
+import { Task } from './task.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity('tags')
 export class Tag {
+  @ApiProperty({ example: 1 })
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ type: 'varchar', length: 50, unique: true })
+  @ApiProperty({ example: 'backend' })
+  @Column({ type: 'varchar', length: 50 })
   name: string;
 
   @ManyToMany(() => Task, (task) => task.tags)
-  tasks: Task[];
+  tasks?: Task[];
 }
